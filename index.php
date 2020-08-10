17f538a45293cb563a1d945f8be33bd75af43163
becode <yf_be@yahoo.co.uk>
2020-08-10 11:13:18 +0200

    added pages to index + import database

--- index.php
+++ index.php
@@ -1,19 +1,75 @@
 <?php
 declare(strict_types=1);
 
-//include all your model files here
-require 'Model/User.php';
-//include all your controllers here
+//include all model files
+require 'Model/DatabaseLoader.php';
+require 'Model/ClassGroup.php';
+require 'Model/Person.php';
+require 'Model/Student.php';
+require 'Model/Teacher.php';
+//include all controllers
 require 'Controller/HomepageController.php';
-require 'Controller/InfoController.php';
-
-//you could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
-//this file should never be more than 20 lines of code!
+require 'Controller/ClassGroupController.php';
+require 'Controller/ClassGroupDetailController.php';
+require 'Controller/ClassGroupFormController.php';
+require 'Controller/StudentController.php';
+require 'Controller/StudentDetailController.php';
+require 'Controller/StudentFormController.php';
+require 'Controller/TeacherController.php';
+require 'Controller/TeacherDetailController.php';
+require 'Controller/TeacherFormController.php';
 
 $controller = new HomepageController();
-if(isset($_GET['page']) && $_GET['page'] === 'info') {
-    $controller = new InfoController();
+if(isset($_GET['page'])) {
+    switch ($_GET['page']) {
+        case 'class':
+            $controller = new ClassGroupController();
+            if (isset($_GET['id'])) {
+                $controller = new ClassGroupDetailController();
+            }
+            if(isset($_POST['action'])) {
+                switch ($_POST['action']){
+                    case 'delete':
+                        break;
+                    case 'add':
+                    case 'update':
+                        $controller = new ClassGroupFormController();
+                        break;
+                }
+            }
+            break;
+        case 'teacher':
+            $controller = new TeacherController();
+            if (isset($_GET['id'])) {
+                $controller = new TeacherDetailController();
+            }
+            if(isset($_POST['action'])) {
+                switch ($_POST['action']){
+                    case 'delete':
+                        break;
+                    case 'add':
+                    case 'update':
+                        $controller = new TeacherFormController();
+                        break;
+                }
+            }
+            break;
+        case 'student':
+            $controller = new StudentController();
+            if (isset($_GET['id'])) {
+                $controller = new StudentDetailController();
+            }
+            if(isset($_POST['action'])) {
+                switch ($_POST['action']){
+                    case 'delete':
+                        break;
+                    case 'add':
+                    case 'update':
+                        $controller = new StudentFormController();
+                        break;
+                }
+            }
+            break;
+    }
 }
-
-
 $controller->render($_GET, $_POST);
