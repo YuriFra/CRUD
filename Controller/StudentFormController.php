05e7985ec9ed3ae639b9d7fa1448547c2593ff10
becode <yf_be@yahoo.co.uk>
2020-08-11 09:34:29 +0200

    delete db file

--- Controller/StudentFormController.php
+++ Controller/StudentFormController.php
@@ -3,14 +3,38 @@ declare(strict_types = 1);
 
 class StudentFormController
 {
-    //render function with both $_GET and $_POST vars available if it would be needed.
     public function render(array $GET, array $POST)
     {
-        //this is just example code, you can remove the line below
+        if($_POST['action'] === 'add') {
+            $student = new Student("", "", "", "", );
+            if (isset($_POST['id']) && empty($_POST['id'])) {
+                $firstName = htmlspecialchars($_POST['firstName']);
+                $lastName = htmlspecialchars($_POST['lastName']);
+                $address = htmlspecialchars($_POST['address']);
+                $email = htmlspecialchars($_POST['email']);
+                $class_id = $_POST['class_id'];
+                $student = new Student($firstName, $lastName, $address, $email, (int)$class_id);
+                $student->save();
+                $msg = "Data saved.";
+            }
+            $action = 'add';
 
-
-        //you should not echo anything inside your controller - only assign vars here
-        // then the view will actually display them.
+        } else {
+            $action ='update';
+            $loader = new DatabaseLoader();
+            $students = $loader->getStudents();
+            $student = $students[(int)$_POST['id']];
+            if (isset($_POST['submit'])) {
+                $firstName = htmlspecialchars($_POST['firstName']);
+                $lastName = htmlspecialchars($_POST['lastName']);
+                $address = htmlspecialchars($_POST['address']);
+                $email = htmlspecialchars($_POST['email']);
+                $class_id = $_POST['class_id'];
+                $student = new Student($firstName, $lastName, $address, $email, (int)$class_id, (int)$_POST['id']);
+                $student->save();
+                $msg = "Update saved.";
+            }
+        }
 
         //load the view
         require 'View/student_form.php';
--- Model/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-
-
-define('DB_USER', 'yoann');
-define('DB_PASS', 'yoann');
-define('DB_NAME', 'crud_db');
