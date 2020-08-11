d645b154a780961306e352bc66aa4a288420ac8f
Yoann Stas <yoann.stas@me.com>
2020-08-11 16:19:23 +0200

    css & homepage added

--- View/homepage.php
+++ View/homepage.php
@@ -2,8 +2,47 @@
 <!-- this is the view, try to put only simple if's and loops here.
 Anything complex should be calculated in the model -->
 <section>
-    <h4>Hello,</h4>
+<div class="container">
+    <br>
+    <br>
+    <h1 class="text-center font-weight-bold">Welcome to the Becode platform.<br> Here you can alter students, teachers & classes.</h1>
+    <br>
+    <h3 class="text-center ">What category do you want to alter?</h3>
+    <br>
+    <br>
 
-    <p>Put your content here.</p>
+    <div class="card-deck d-flex justify-content-center">
+    <div class="card text-white text-center bg-success mb-3" style="max-width: 18rem;">
+        <div class="card-header"><i class="fa fa-graduation-cap fa-2x"></i></div>
+        <div class="card-body">
+            <h5 class="card-title font-weight-bold">Students</h5>
+            <p class="card-text">Add, edit or delete students here.</p>
+            <br>
+            <a href="?page=student" class="btn btn-light">GO!</a>
+        </div>
+        </div>
+
+    <div class="card text-white text-center bg-warning mb-3" style="max-width: 18rem;">
+        <div class="card-header"><i class="fas fa-chalkboard-teacher fa-2x"></i></div>
+        <div class="card-body">
+            <h5 class="card-title font-weight-bold">Teachers</h5>
+            <p class="card-text">Add, edit or delete teachers here.</p>
+            <br>
+            <a href="?page=teacher" class="btn btn-light">GO!</a>
+        </div>
+        </div>
+
+    <div class="card text-white text-center bg-info mb-3" style="max-width: 18rem;">
+        <div class="card-header"><i class="fas fa-school fa-2x"></i></div>
+        <div class="card-body">
+            <h5 class="card-title font-weight-bold">Classes</h5>
+            <p class="card-text">Add, edit or delete classes here.</p>
+            <br>
+            <a href="?page=class" class="btn btn-light">GO!</a>
+        </div>
+        </div>
+    </div>
+
+
 </section>
 <?php require 'includes/footer.php'?>
--- View/includes/header.php
+++ View/includes/header.php
@@ -5,14 +5,16 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/f4622adaa6.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>CRUD App</title>
 </head>
 <body>
     <header>
-        <h1 class="text-center">Welcome to BECODE</h1>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="index.php">CRUD</a>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.php">Becode Platform</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
